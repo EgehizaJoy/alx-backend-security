901fc8f2db23c198ff80b7388a68fa853554273b
EgehizaJoy <egehizafuraha@gmail.com>
2025-09-04 15:45:56 +0300

    add geo

--- ip_tracking/ip_tracking/middleware.py
+++ ip_tracking/ip_tracking/middleware.py
@@ -1,9 +1,13 @@
-from .models import RequestLog
+from django.http import HttpResponseForbidden
+from django.core.cache import cache
+from ipgeolocation import IpGeoLocation
+from .models import RequestLog,BlockedIP
 from django.utils.timezone import now
 
 class IPTrackingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.geo = IpGeoLocation()  # initialize the library
 
     def __call__(self, request):
         # Get IP address (works even if behind proxy/load balancer if X-Forwarded-For is set)
@@ -16,12 +20,28 @@ class IPTrackingMiddleware:
         # Block if IP is in blacklist
         if BlockedIP.objects.filter(ip_address=ip_address).exists():
             return HttpResponseForbidden("Your IP has been blocked.")    
+        
+        # Try to get cached geolocation (24h cache)
+        geo_data = cache.get(f"geo:{ip_address}")
 
+        if not geo_data:
+            try:
+                result = self.geo.get_geolocation(ip_address)
+                geo_data = {
+                    "country": result.get("country_name"),
+                    "city": result.get("city"),
+                }
+                cache.set(f"geo:{ip_address}", geo_data, 60 * 60 * 24)  # 24h
+            except Exception:
+                geo_data = {"country": None, "city": None}
+                
         # Log to database
         RequestLog.objects.create(
             ip_address=ip_address,
             timestamp=now(),
-            path=request.path
+            path=request.path,
+              country=geo_data.get("country"),
+            city=geo_data.get("city"),
         )
 
         # Continue processing request
--- ip_tracking/ip_tracking/models.py
+++ ip_tracking/ip_tracking/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
 
 class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+
 
     def __str__(self):
         return f"{self.ip_address} - {self.path} @ {self.timestamp}"
